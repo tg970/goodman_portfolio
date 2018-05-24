fad93c806e7a358416010e1c2143e23bc430da68
tg970 <tgoodman@outlook.com>
2018-05-24 15:49:30 -0500

    gulp css running, commented out rest of code to slowly step into it

--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,14 @@ const del = require('del');
 const pump = require('pump');
 
 const scripts = require('./src/scripts');
-const appScripts = require('./src/appScripts');
+//const dependencies = require('./src/depends');
 const styles = require('./src/styles');
 
-var devMode = false;
 var minify = composer(uglifyjs, console);
 
-gulp.task('clean', function() {
-  return del(['build']);
-});
+// gulp.task('clean', function() {
+//   return del(['build']);
+// });
 
 gulp.task('css', function() {
   gulp.src(styles)
@@ -34,82 +33,82 @@ gulp.task('css', function() {
     }));
 });
 
-gulp.task('js', ['clean'], function(cb) {
-  pump([
-    gulp.src(scripts),
-    minify(),
-    concat('scripts.js'),
-    gulp.dest('./public/js')
-  ], cb);
-});
-
-gulp.task('appJS', ['clean'], function(cb) {
-  pump([
-    gulp.src(appScripts),
-    sourcemaps.init(),
-    concat('appScripts.js'),
-    sourcemaps.write(),
-    gulp.dest('./public/js'),
-    browserSync.reload({
-      stream: true
-    })
-  ], cb);
-});
-
-gulp.task('html', function() {
-  gulp.src('./src/html/**/*.html')
-    .pipe(gulp.dest('./public'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
-
-gulp.task('images', ['clean'], function() {
-  return gulp.src('./src/assets/**/*.*')
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('./public/assets'));
-});
-
-gulp.task('builder', function() {
-  gulp.start(['css','appJS','html', 'images']);
-});
-
-gulp.task('serve', ['browser-sync'], function() {
-  gulp.watch('./src/css/**/*.css', ['css']);
-  gulp.watch('./src/js/**/*.js', ['appJS']);
-  gulp.watch('./src/html/**/*.html', ['html']);
-  gulp.watch('./src/assets/**/*.*', ['images']);
-});
-
-gulp.task('browser-sync', ['nodemon'], function() {
-  browserSync.init(null, {
-    proxy: "http://localhost:3000",
-    port: 1122,
-    open: false
-  });
-});
-
-gulp.task('nodemon', [], function (done) {
-	var running = false;
-
-	return nodemon({
-		script: 'server.js',
-    watch: [
-      'controllers/**/*.*',
-      'models/**/*.*',
-      'data/**/*.*',
-      'server.js'
-    ],
-    ignore: [ 'gulpfile.js', 'node_modules/' ]
-	}).on('start', function () {
-		if (!running) done();
-    running = true;
-	})
-  .on('restart', function () {
-    setTimeout(function () {
-      reload();
-    }, 1000);
-  });
-});
-
-gulp.task('start', ['builder', 'serve']);
+// gulp.task('js', ['clean'], function(cb) {
+//   pump([
+//     gulp.src(scripts),
+//     minify(),
+//     concat('scripts.js'),
+//     gulp.dest('./public/js')
+//   ], cb);
+// });
+//
+// gulp.task('appJS', ['clean'], function(cb) {
+//   pump([
+//     gulp.src(appScripts),
+//     sourcemaps.init(),
+//     concat('appScripts.js'),
+//     sourcemaps.write(),
+//     gulp.dest('./public/js'),
+//     browserSync.reload({
+//       stream: true
+//     })
+//   ], cb);
+// });
+//
+// gulp.task('html', function() {
+//   gulp.src('./src/html/**/*.html')
+//     .pipe(gulp.dest('./public'))
+//     .pipe(browserSync.reload({
+//       stream: true
+//     }));
+// });
+//
+// gulp.task('images', ['clean'], function() {
+//   return gulp.src('./src/assets/**/*.*')
+//     .pipe(imagemin({optimizationLevel: 5}))
+//     .pipe(gulp.dest('./public/assets'));
+// });
+//
+// gulp.task('builder', function() {
+//   gulp.start(['css','appJS','html', 'images']);
+// });
+//
+// gulp.task('serve', ['browser-sync'], function() {
+//   gulp.watch('./src/css/**/*.css', ['css']);
+//   gulp.watch('./src/js/**/*.js', ['appJS']);
+//   gulp.watch('./src/html/**/*.html', ['html']);
+//   gulp.watch('./src/assets/**/*.*', ['images']);
+// });
+//
+// gulp.task('browser-sync', ['nodemon'], function() {
+//   browserSync.init(null, {
+//     proxy: "http://localhost:3000",
+//     port: 1122,
+//     open: false
+//   });
+// });
+//
+// gulp.task('nodemon', [], function (done) {
+// 	var running = false;
+//
+// 	return nodemon({
+// 		script: 'server.js',
+//     watch: [
+//       'controllers/**/*.*',
+//       'models/**/*.*',
+//       'data/**/*.*',
+//       'server.js'
+//     ],
+//     ignore: [ 'gulpfile.js', 'node_modules/' ]
+// 	}).on('start', function () {
+// 		if (!running) done();
+//     running = true;
+// 	})
+//   .on('restart', function () {
+//     setTimeout(function () {
+//       reload();
+//     }, 1000);
+//   });
+// });
+//
+// gulp.task('start', ['builder', 'serve']);
