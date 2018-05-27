233de07658327c9ad85e7cd20fb8a947502ab674
tg970 <tgoodman@outlook.com>
2018-05-26 21:03:22 -0500

    route provider in, but doing weird things to home html as a partial

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
-const app = angular.module('goodman_app', []);
+const app = angular.module('goodman_app', ['ngRoute']);
 
-app.controller('NavController', ['$http', '$scope', '$location',function($http, $scope, $location) {
+app.controller('NavController', ['$http', '$scope', '$location', function($http, $scope, $location) {
   this.test = 'Yo Yo, its working';
+}]);
+
+
+app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
+  $locationProvider.html5Mode({ enabled: true });
+
+  $routeProvider.when('/', {
+    templateUrl: 'pages/home.html',
+    // controller: 'NavController as ctrl',
+    // controllerAs: 'ctrl'
+  });
+
 }]);
